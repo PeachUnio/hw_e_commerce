25d786b851ad1ed9afbb5bf38f744ce152b2c786
Арина Новгородова <arrrishanov@gmail.com>
2025-08-15 17:57:25 +0300

    update class Product

--- src/classes.py
+++ src/classes.py
@@ -1,14 +1,33 @@
 class Product:
     name: str
     description: str
-    price: float
+    __price: float
     quantity: int
 
     def __init__(self, name, description, price, quantity):
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, data_list):
+        return cls(name=data_list["name"],
+                      description=data_list["description"],
+                      price=data_list["price"],
+                      quantity=data_list["quantity"])
+
+    @property
+    def price(self):
+        return self.__price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Цена не должна быть нулевая или отрицательная")
+        else:
+            self.__price = new_price
+
 
 
 class Category:
