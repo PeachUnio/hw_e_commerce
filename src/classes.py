28f50f47252991991497d901bc0a7ee851a7e720
Арина Новгородова <arrrishanov@gmail.com>
2025-08-23 12:36:13 +0300

    add new classes Smartphone and LawnGrass

--- src/classes.py
+++ src/classes.py
@@ -84,3 +84,34 @@ class Category:
             product_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
 
         return product_str
+
+
+
+class Smartphone(Product):
+    def __init__(self, name: str,
+                 description: str,
+                 price: float,
+                 quantity: int,
+                 efficiency: float,
+                 model: str,
+                 memory: int,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.efficiency = efficiency
+        self.model = model
+        self.memory = memory
+        self.color = color
+
+
+class LawnGrass(Product):
+    def __init__(self, name: str,
+                 description: str,
+                 price: float,
+                 quantity: int,
+                 country: str,
+                 germination_period: str,
+                 color: str):
+        super().__init__(name, description, price, quantity)
+        self.country = country
+        self.germination_period = germination_period
+        self.color = color
