d54ad07327eb1a4097a0eaddefe33ce1701f55f5
Арина Новгородова <arrrishanov@gmail.com>
2025-08-29 14:48:40 +0300

    add Mixin class wis repr function

--- src/classes.py
+++ src/classes.py
@@ -11,7 +11,13 @@ class BaseProduct(ABC):
         pass
 
 
-class Product(BaseProduct):
+class MixinProduct:
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}('{self.name}', '{self.description}', {self.price}, {self.quantity})"
+
+
+class Product(BaseProduct, MixinProduct):
     """Класс для продуктов"""
 
     name: str
