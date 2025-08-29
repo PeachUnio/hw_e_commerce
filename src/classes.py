1afc194a673006e1025d8d494ad2d177cde15265
Арина Новгородова <arrrishanov@gmail.com>
2025-08-29 16:46:02 +0300

    update MixinProduct

--- src/classes.py
+++ src/classes.py
@@ -16,6 +16,9 @@ class BaseProduct(ABC):
 class MixinProduct:
     """Миксин, добавляющий функционал для repr()"""
 
+    def __int__(self):
+        print(repr(self))
+
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', '{self.description}', {self.price}, {self.quantity})"
 
